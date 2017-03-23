950db6b3eab2f82e31ff0bef112d37e0b4130ad6
dimakrol <dima.krol@mail.ru>
2017-03-23 15:07:51 +0200

    Add payment testing.

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Billing\PaymentFailedException;
 use App\Billing\PaymentGateway;
 use App\Concert;
 use Illuminate\Http\Request;
@@ -22,10 +23,16 @@ class ConcertOrdersController extends Controller
             'ticket_quantity' => "required|integer|min:1",
             'payment_token' => 'required'
         ]);
-        $concert = Concert::find($concertId);
-        $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
-        $concert->orderTickets(request('email'), request('ticket_quantity'));
 
-        return response()->json([], 201);
+        try {
+            $concert = Concert::find($concertId);
+            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+            $concert->orderTickets(request('email'), request('ticket_quantity'));
+
+            return response()->json([], 201);
+        } catch (PaymentFailedException $e) {
+            return response()->json([], 422);
+        }
+
     }
 }
